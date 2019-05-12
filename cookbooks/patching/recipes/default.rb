6065fbe0380e6617c6ef2b0bcc314daac43b8944
Tim Smith <tsmith@chef.io>
2019-05-12 13:05:24 -0700

    Use platform family to simplify code a bit
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- cookbooks/patching/recipes/default.rb
+++ cookbooks/patching/recipes/default.rb
@@ -2,20 +2,22 @@
 # Cookbook:: patching
 # Recipe:: default
 #
-# Copyright:: 2018, The Authors, All Rights Reserved.
+# Copyright:: 2019, The Authors, All Rights Reserved.
 
 patch_sentinel = File.join(Chef::Config.file_cache_path, "chef_patching_sentinel")
 
-case node['platform']
-when 'debian', 'ubuntu'
+case node['platform_family']
+when 'debian'
   execute 'apt-get upgrade -y'
+  
   file patch_sentinel do
     mode "0600"
     content "#{node['patching']['patchlevel']}"
   end
 
-when 'redhat', 'centos', 'fedora', 'amazon'
+when 'rhel', 'fedora', 'amazon'
   execute 'yum update -y'
+  
   file patch_sentinel do
     mode "0600"
     content "#{node['patching']['patchlevel']}"
