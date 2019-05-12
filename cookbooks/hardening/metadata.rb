bcffcef2141ebb4c7054c7e6b9a0e5a3b603c6c7
Tim Smith <tsmith@chef.io>
2019-05-12 13:05:10 -0700

    Require Chef 13 or later
    This avoids the need for the responds_to bit
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- cookbooks/hardening/metadata.rb
+++ cookbooks/hardening/metadata.rb
@@ -5,7 +5,7 @@ license 'All Rights Reserved'
 description 'Installs/Configures hardening'
 long_description 'Installs/Configures hardening'
 version '0.1.0'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 13'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
--- cookbooks/patching/metadata.rb
+++ cookbooks/patching/metadata.rb
@@ -5,7 +5,7 @@ license 'All Rights Reserved'
 description 'Installs/Configures patching'
 long_description 'Installs/Configures patching'
 version '0.1.0'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 13'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
