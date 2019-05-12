75c9aae298cc52b5a26fedaaa02ae7f1764eb2aa
Tim Smith <tsmith@chef.io>
2019-05-12 13:17:33 -0700

    Cookstyle fixes
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- cookbooks/hardening/recipes/remediation.rb
+++ cookbooks/hardening/recipes/remediation.rb
@@ -1,5 +1,5 @@
-package "%w(rsyslog audit)
+package %w(rsyslog audit)
 
-cookbook_file "/etc/audit/auditd.conf" do
-  source "auditd.conf"
+cookbook_file '/etc/audit/auditd.conf' do
+  source 'auditd.conf'
 end
--- cookbooks/patching/recipes/default.rb
+++ cookbooks/patching/recipes/default.rb
@@ -4,22 +4,22 @@
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
-patch_sentinel = File.join(Chef::Config.file_cache_path, "chef_patching_sentinel")
+patch_sentinel = File.join(Chef::Config.file_cache_path, 'chef_patching_sentinel')
 
 case node['platform_family']
 when 'debian'
   execute 'apt-get upgrade -y'
-  
+
   file patch_sentinel do
-    mode "0600"
-    content "#{node['patching']['patchlevel']}"
+    mode '0600'
+    content node['patching']['patchlevel'].to_s
   end
 
 when 'rhel', 'fedora', 'amazon'
   execute 'yum update -y'
-  
+
   file patch_sentinel do
-    mode "0600"
-    content "#{node['patching']['patchlevel']}"
+    mode '0600'
+    content node['patching']['patchlevel'].to_s
   end
 end
